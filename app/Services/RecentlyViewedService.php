993228a299ea0b72ee3869296404385d18071442
TheMaksBlock <maka228228@mail.ru>
2024-04-22 23:36:35 +0300

    fixed bug with recently viewed

--- app/Services/RecentlyViewedService.php
+++ app/Services/RecentlyViewedService.php
@@ -17,30 +17,32 @@ class RecentlyViewedService {
             $recentlyViewed = explode(',', $recentlyViewed);
             if(!in_array($id, $recentlyViewed)){
                 $recentlyViewed[] = $id;
-
                 Cookie::queue('recentlyViewed', implode(',', $recentlyViewed), 60*24);
             }
         }
     }
 
-    public function getRecentlyViewed()
+    public function getRecentlyViewed($id)
     {
         $recentlyViewed = Cookie::get('recentlyViewed');
         if(!empty($recentlyViewed)){
             $recentlyViewed = explode(',', $recentlyViewed);
+
+            $key = array_search($id, $recentlyViewed);
+            if ($key !== false) {
+                unset($recentlyViewed[$key]);
+            }
+
             return array_slice($recentlyViewed, -3);
         } else {
             return false;
         }
     }
 
-    public function getRecentlyViewedProducts(){
-        $ids = $this->getRecentlyViewed();
+    public function getRecentlyViewedProducts($id){
+        $ids = $this->getRecentlyViewed($id);
         if($ids){
             return Product::whereIn("id",$ids)->get();
         }
     }
-
-
-
 }
